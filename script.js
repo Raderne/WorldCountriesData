895c53134a66ea1f6ad07114a6bd69e28192c247
Reda El Marzouki <relmarzouki@gmail.com>
2023-05-27 12:35:40 +0300

    added search func

--- script.js
+++ script.js
@@ -1,38 +1,74 @@
-const numOfCountries = document.getElementById("numOfCountries");
 const countriesWrapper = document.querySelector(".countries-wrapper");
-const countriesAPI = "https://restcountries.com/v2/all";
+const searchInput = document.getElementById("search");
+const numOfCriteria = document.getElementById("numOfCriteria");
 
-function countriesHtml(country) {
-  let language = country.languages.map((lan) => {
-    return lan.name + " ";
-  });
-  return `
-        <div class="box">
+const countriesAPI =
+  "https://restcountries.com/v2/all?fields=name,flags,capital,languages,population";
+
+let userInput = "";
+
+function showCountries(country) {
+  let language = country.languages.map((lan) => lan.name + " ");
+
+  let box = document.createElement("div");
+  box.classList.add("box");
+
+  box.innerHTML = `
             <img src="${country.flags.png}" alt="flag" id="flag">
             <h3 id="country-name">${country.name}</h3>
             <div class="info">
-                <p id="capital">capital: <span>${
-                  country.capital !== undefined ? country.capital : ""
+                <p id="capital"><span>${
+                  country.capital !== undefined
+                    ? "capital: " + country.capital
+                    : ""
                 }</span></p>
                 <p id="languages">languages: <span>${language}</span></p>
                 <p id="population">population: <span>${
                   country.population
                 }</span></p>
             </div>
-        </div>
     `;
+
+  countriesWrapper.appendChild(box);
 }
 
-function countriesData() {
-  fetch(countriesAPI)
+getCountriesData(countriesAPI, null);
+
+function getCountriesData(countriesApi, search) {
+  countriesWrapper.innerHTML = "";
+  fetch(countriesApi)
     .then((resp) => resp.json())
     .then((data) => {
-      console.log(data[8]);
-      numOfCountries.innerText = data.length;
-
       data.forEach((country) => {
-        countriesWrapper.innerHTML += countriesHtml(country);
+        let language = "";
+        country.languages.map((lan) => {
+          language += lan.name + ", ";
+        });
+        if (
+          (country.capital != null && country.capital.match(search)) ||
+          language.match(search)
+        ) {
+          numOfCriteria.parentElement.classList.add("active");
+          numOfCriteria.innerText = countriesWrapper.children.length + 1;
+
+          showCountries(country);
+        } else if (search === null) {
+          numOfCriteria.parentElement.classList.remove("active");
+          showCountries(country);
+        }
       });
     });
 }
-countriesData();
+
+searchInput.addEventListener("input", (e) => {
+  e.preventDefault();
+
+  userInput = e.target.value;
+  let searchTarget = new RegExp(userInput, "gi");
+
+  if (userInput && userInput !== "") {
+    getCountriesData(countriesAPI, searchTarget);
+  } else {
+    window.location.reload();
+  }
+});
